215fb49b694ff5eed81e7645ff28fbf5ffc1342b
padmin <paul.kraus1@veritas.com>
2016-04-15 14:32:51 -0700

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# profile_dsc
+## PURPOSE:
+
+Configues DSC to work with Puppet
+
+## HIERA DATA:
+
+ profile::dsc:lcm_config:
+
+ Using PuppetLabs recommendation:
+
+ https://forge.puppetlabs.com/puppetlabs/dsc#optionally-configure-the-dsc-lcm-refreshmode
+
+## HIERA EXAMPLE:
+```
+profile::dsc:lcm_config: 'Disabled'
+
+```
+
+## MODULE DEPENDENCIES:
+```
+puppet module install puppetlabs-dsc
+```
+## USAGE:
+
+#### Puppetfile:
+```
+mod 'puppetlabs-dsc',
+  :git => 'https://github.com/puppetlabs/puppetlabs-dsc',
+  :tag => '1.0.0'
+
+mod 'validation_script',
+  :git => 'https://github.com/firechiefs/validation_script',
+  :ref => '1.0.0'
+
+mod 'profile_dsc',
+  :git => 'https://github.com/firechiefs/profile_dsc',
+  :ref => '1.0.0'
+```
+#### Manifests:
+```
+class role::*rolename* {
+  include profile_dsc
+}
+```
---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+# Configues DSC to work with Puppet
+
+# HIERA DATA:
+# profile::dsc:lcm_config:
+#
+# Using PuppetLabs recommendation:
+# https://forge.puppetlabs.com/puppetlabs/dsc#optionally-configure-the-dsc-lcm-refreshmode
+
+# HIERA EXAMPLE:
+# profile::dsc:lcm_config: 'Disabled'
+
+class profile::dsc {
+  # HIERA LOOKUP:
+  # --> PUPPET CODE VARIABLES:
+  $lcm_config_refresh_mode = hiera('profile::dsc::lcm_config_refresh_mode')
+  validate_string($lcm_config_refresh_mode)
+
+  # Set Windows DSC LCM refresh_mode
+  dsc::lcm_config {'disable_lcm':
+    refresh_mode => $lcm_config_refresh_mode,
+  }
+
+  # VALIDATION CODE:
+  # --> MODIFY VARIABLES BELOW:
+  $profile_name    = 'profile_dsc'                    # set to profile name
+  $validation_data = $lcm_config_refresh_mode # validate DSC refresh_mode
+
+  # Puppet custom define type
+  # documented in: site/validation_script/manifests/init.pp
+  # DO NOT MODIFY BELOW !!!
+  validation_script { $profile_name:
+    profile_name    => $profile_name,
+    validation_data => $validation_data,
+  }
+
+}
